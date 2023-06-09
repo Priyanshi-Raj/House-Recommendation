9b3d5a739255875ce7482280a163934ef90a9a04
Priyanshi-Raj <priyanshiraj2909@gmail.com>
2023-06-09 13:43:35 +0530

    updation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,29 +26,5 @@ const Home = () => {
 };
 
 export default Home;
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
 
-// const Home = () => {
-//   const navigate = useNavigate();
-
-//   const handleJoinUs = () => {
-//     navigate('/house-layout');
-//   };
-
-//   return (
-//     <div className="container-home">
-//       <div className="left-section">
-//         <h2>Want to get the best recommendation for your house by creating your own layout?</h2>
-//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo saepe autem nam doloribus itaque similique modi, consequuntur aperiam maxime quas?</p>
-//         <button onClick={handleJoinUs} className="join-button">Join Us</button>
-//       </div>
-//       <div className="right-section">
-//         <img src="https://wpmedia.roomsketcher.com/content/uploads/2022/01/05111052/Apartment-3D-floor-plan-780x670-1.png" alt="House" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
 
